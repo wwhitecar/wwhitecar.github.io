2cb85a4dc0e47385d6ec2a8733667281ce632c83
unknown <wwhiteca@msudenver.edu>
2017-11-17 19:23:17 -0700

    moving image to elm

--- Main.elm
+++ Main.elm
@@ -2,7 +2,10 @@ module Main exposing(..)
 
 import Html exposing (Html, span, text)
 import Html.Attributes exposing (class)
+import Element exposing (..)
+
+img = image 960 440 "http://prod.static.cardinals.clubs.nfl.com/nfl-assets/img/gbl-ico-team/ARI/logos/home/large.png"
 
 main : Html a
 main = 
-    span [ class "welcome-message" ] [ text "Hello, World!" ]
+    span [ class "welcome-message" ] [ text "Hello, World!", img ] 
